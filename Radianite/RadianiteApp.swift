2dd0e5ebf483f3eeaf2196b62827e43595c96af2
abeaudoin <abeaudoin@ippon.fr>
2022-10-17 18:06:43 +0200

    init: xcode project created

---
+++ Radianite/RadianiteApp.swift
@@ -0,0 +1,17 @@
+//
+//  RadianiteApp.swift
+//  Radianite
+//
+//  Created by ippon on 17/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct RadianiteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
